7412c15fb566bf4b7b88d443399aaa3e64b91089
Jaydon <pearcejaydon@gmail.com>
2022-09-04 20:22:45 +1000

    Add try/catch

--- script.js
+++ script.js
@@ -3,12 +3,17 @@ const searchTerm = document.getElementById("search");
 const searchButton = document.getElementById("searchBtn");
 
 async function getGif(query) {
-  const response = await fetch(
-    `https://api.giphy.com/v1/gifs/translate?api_key=EgaopUnqXyuyq1n4ENNvKO9fejnLjR33&s=${query}`,
-    { mode: "cors" }
-  );
-  const gifData = await response.json();
-  img.src = gifData.data.images.original.url;
+  try {
+    const response = await fetch(
+      `https://api.giphy.com/v1/gifs/translate?api_key=EgaopUnqXyuyq1n4ENNvKO9fejnLjR33&s=${query}`,
+      { mode: "cors" }
+    );
+    const gifData = await response.json();
+    img.src = gifData.data.images.original.url;
+  } catch (error) {
+    console.log(error);
+    img.src = "default.jpg";
+  }
 }
 
 searchButton.addEventListener("click", () => {
